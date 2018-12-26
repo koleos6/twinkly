04befcb9c1b2d88982a4dc9ab7a1f6d48787902c
koleos6 <forum@wils.fr>
2018-12-26 23:54:36 +0100

    Clean-up et documentation

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -23,30 +23,10 @@ if (!isConnect()) {
 }
 ?>
 
-<!--
+
 <form class="form-horizontal">
     <fieldset>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 1}}</label>
-            <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param1" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
-            <div class="col-lg-2">
-                <input class="configKey form-control" data-l1key="param2" value="80" />
-            </div>
-        </div>
-        <div class="form-group">
-            <label class="col-lg-4 control-label">{{Global param 2}}</label>
-            <div class="col-lg-2">
-                <select class="configKey form-control" data-l1key="param3">
-                    <option value="value1">value1</option>
-                    <option value="value2">value2</option>
-                </select>
-            </div>
-        </div>
+        <div class="alert alert-success">{{Prérequis: il faut au préalable avoir configuré via l'application Twinkly la guirlande sur le même réseau que Jeedom et lui avoir attribué une IP fixe.}}</div>
   </fieldset>
 </form>
--->
+
